1668f9011cad7229344b97c109799766246a456a
alee-tsr <tanseri.alee@gmail.com>
2025-12-05 11:59:12 +0100

    test super Maire

--- PROJET_FRIPOUILLES/src/Maire.java
+++ PROJET_FRIPOUILLES/src/Maire.java
@@ -1,3 +1,6 @@
-public class Maire {
+public class Maire extends Utilisateur {
 
+    public Maire(int unIdUser, String unLogin, String unMdp, String unNom, String unPrenom, String unMail, String unRole) {
+        super(unIdUser, unLogin, unMdp, unNom, unPrenom, unMail, unRole);
+    }
 }

84d4fc7c2460d21c103157602ee7444dde906b08
adam <adam@gmail.com>
2025-12-05 11:32:46 +0100

    Consoleview

--- PROJET_FRIPOUILLES/src/ConsoleView.java
+++ PROJET_FRIPOUILLES/src/ConsoleView.java
@@ -1,10 +1,25 @@
+import java.util.List;
+import java.util.Scanner;
 
 public class ConsoleView {
+    private Scanner scanner = new Scanner(System.in);
 
-	private String mamadou;
-	
-	public ConsoleView(String unMamadou) {
-		mamadou = unMamadou;
-	}
-	
+    public void afficherMsg(String msg) { System.out.println(">>> " + msg); }
+    
+    public String saisirTexte(String question) {
+        System.out.print(question + " : ");
+        return scanner.nextLine();
+    }
+
+    public double saisirDouble(String question) {
+        System.out.print(question + " : ");
+        double val = scanner.nextDouble();
+        scanner.nextLine(); // Vider le buffer
+        return val;
+    }
+
+    public void afficherListe(List<?> liste) {
+        if(liste.isEmpty()) System.out.println("(Aucun élément)");
+        for(Object o : liste) { System.out.println("- " + o.toString()); }
+    }
 }
